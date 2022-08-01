d810ff8475fdc7af552d22ae66a4d1291805f2fb
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-08-01 17:30:50 +0700

    refactor: dependency to Post

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -2,18 +2,14 @@
 
 namespace frontend\controllers;
 
-use common\models\Post;
+
+use frontend\resource\Post;
 use yii\rest\ActiveController;
 
 class PostController extends ActiveController {
 
     // controller post ini akan menggunakan model post
     public $modelClass = Post::class;
-
-    public function actionCreate()
-    {
-        $model = new Post();
-    }
 
 }
 
